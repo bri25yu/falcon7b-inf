0253b7e2d7296d7b71e2325b2297a6174d4c8110
bri25yu <bri25yu@gmail.com>
2023-06-11 17:13:28 -0700

    reverting old logic

--- modeling_falcon.py
+++ modeling_falcon.py
@@ -54,7 +54,6 @@ def apply_rotary(embeds, cos, sin):
     return embeds * cos + embeds_half_rotated * sin
 
 
-"""
 class RotaryEmbedding(Module):
     def __init__(self, config: RWConfig, base: float=10000) -> None:
         super().__init__()
@@ -102,61 +101,6 @@ class RotaryEmbedding(Module):
             sin = sin[:, :, :L, :]
 
         return apply_rotary(query, cos, sin), apply_rotary(key, cos, sin)
-"""
-
-import torch
-def rotate_half(x):
-    x1, x2 = x[..., : x.shape[-1] // 2], x[..., x.shape[-1] // 2 :]
-    return torch.cat((-x2, x1), dim=x1.ndim - 1)  # dim=-1 triggers a bug in torch < 1.8.0
-
-
-class RotaryEmbedding(torch.nn.Module):
-    """Implementation of RotaryEmbedding from GPT-NeoX.
-    This implementation is design to operate on queries and keys that are compatible with
-    [batch_size, n_heads_per_partition, seq_len, head_dim] (e.g. MinGPTAttention format).
-    """
-
-    def __init__(
-        self,
-        head_dim: int,
-        base=10000,
-    ):
-        super().__init__()
-        inv_freq = 1.0 / (base ** (torch.arange(0, head_dim, 2).float() / head_dim))
-        self.register_buffer("inv_freq", inv_freq, persistent=False)
-        self.head_dim = head_dim
-        self.seq_len_cached = None
-        self.batch_size_cached = None
-        self.cos_cached: torch.Tensor | None = None
-        self.sin_cached: torch.Tensor | None = None
-
-    def cos_sin(
-        self,
-        seq_len: int,
-        device="cuda",
-        dtype=torch.bfloat16,
-    ) -> torch.Tensor:
-        if seq_len != self.seq_len_cached:
-            self.seq_len_cached = seq_len
-            t = torch.arange(seq_len, device=device).type_as(self.inv_freq)
-            freqs = torch.einsum("i,j->ij", t, self.inv_freq)
-            emb = torch.cat((freqs, freqs), dim=-1).to(device)
-
-            if dtype in [torch.float16, torch.bfloat16]:
-                emb = emb.float()
-
-            self.cos_cached = emb.cos()[None, :, :]
-            self.sin_cached = emb.sin()[None, :, :]
-
-            self.cos_cached = self.cos_cached.type(dtype)
-            self.sin_cached = self.sin_cached.type(dtype)
-
-        return self.cos_cached, self.sin_cached
-
-    def forward(self, q, k):
-        batch, seq_len, head_dim = q.shape
-        cos, sin = self.cos_sin(seq_len, q.device, q.dtype)
-        return (q * cos) + (rotate_half(q) * sin), (k * cos) + (rotate_half(k) * sin)
 
 
 class Attention(Module):
@@ -171,8 +115,7 @@ class Attention(Module):
         if Dkv * H != D:
             raise ValueError(f"`hidden_size` must be divisible by num_heads (got `hidden_size`: {D} and `num_heads`: {H}).")
 
-        # self.maybe_rotary = RotaryEmbedding(config)
-        self.maybe_rotary = RotaryEmbedding(Dkv)
+        self.maybe_rotary = RotaryEmbedding(config)
         self.query_key_value = Linear(D, D + 2 * Nkv * Dkv, dtype=config.torch_dtype)
         self.dense: DD = Linear(D, D, dtype=config.torch_dtype)
 
@@ -198,13 +141,7 @@ class Attention(Module):
         query: NHLDkv = fused_qkv[:, :, :-2 * Nkv, :].transpose(1, 2)
         key: NHLDkv = fused_qkv[:, :, -2 * Nkv: -Nkv, :].transpose(1, 2)
         value: NHLDkv = fused_qkv[:, :, -Nkv:, :].transpose(1, 2)
-        # query, key = self.maybe_rotary(query, key, past_key_value_length)
-
-        query = query.view(-1, L, Dkv)
-        key = key.view(-1, L, Dkv)
-        query, key = self.maybe_rotary(query, key)
-        query = query.view(N, H, L, Dkv)
-        key = key.view(N, Nkv, L, Dkv)
+        query, key = self.maybe_rotary(query, key, past_key_value_length)
 
         if using_past_key_values:  # Concatenate after relative positional encoding to not duplicate encoding
             key: NHLDkv = cat((past_key, key), dim=2)
